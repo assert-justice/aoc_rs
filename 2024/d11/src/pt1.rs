c7000cbc745e62ecc82f99d03336c25fc8353459
Riley <waskerdu@gmail.com>
2024-12-11 12:05:04 -0800

    2024 day 11

--- 2024/d11/src/pt1.rs
+++ 2024/d11/src/pt1.rs
@@ -1,7 +1,36 @@
 
+fn blink(data: Vec<u64>) -> Vec<u64>{
+    let mut out = Vec::new();
+    for x in data{
+        let str = x.to_string();
+        if x == 0{
+            out.push(1);
+        }
+        else if str.len() % 2 == 0{
+            let a = &str[0..(str.len()/2)];
+            let b = &str[(str.len()/2)..];
+            out.push(a.parse().unwrap());
+            out.push(b.parse().unwrap());
+        }
+        else{
+            out.push(x * 2024);
+        }
+    }
+    out
+}
+
 #[allow(dead_code)]
 pub fn solve(txt: &str){
     for line in txt.lines(){
-        println!("{line}");
+        let mut xs = line.split(' ')
+            .map(|s|s.parse::<u64>().unwrap()).collect::<Vec<u64>>();
+        // println!("{:?}", xs);
+        // let xs = blink(xs);
+        // println!("{:?}", xs);
+        for _ in 0..25{
+            xs = blink(xs);
+        }
+        println!("{}", xs.len());
+        break;
     }
 }
--- 2024/d11/src/pt2.rs
+++ 2024/d11/src/pt2.rs
@@ -1,7 +1,50 @@
+use std::collections::HashMap;
+
+fn blink(x: u64) -> Vec<u64>{
+    let mut out = Vec::new();
+    let str = x.to_string();
+    if x == 0{
+        out.push(1);
+    }
+    else if str.len() % 2 == 0{
+        let a = &str[0..(str.len()/2)];
+        let b = &str[(str.len()/2)..];
+        out.push(a.parse().unwrap());
+        out.push(b.parse().unwrap());
+    }
+    else{
+        out.push(x * 2024);
+    }
+    out
+}
+
+fn count(mem: &mut HashMap<(u64, usize), usize>, val: u64, step: usize) -> usize{
+    if step == 0{
+        return 1;
+    }
+    if let Some(v) = mem.get(&(val,step)) {
+        return *v;
+    }
+    let mut total = 0;
+    for x in blink(val){
+        total += count(mem, x, step-1);
+    }
+    mem.insert((val,step), total);
+    total
+}
 
 #[allow(dead_code)]
 pub fn solve(txt: &str){
     for line in txt.lines(){
-        println!("{line}");
+        let xs = line.split(' ')
+            .map(|s|s.parse::<u64>().unwrap()).collect::<Vec<u64>>();
+        let mut mem: HashMap<(u64, usize), usize> = HashMap::new();
+        let mut total = 0;
+        for x in xs{
+            total += count(&mut mem, x, 75);
+        }
+        println!("{total}");
+        // println!("{:?}", mem);
+        break;
     }
 }
